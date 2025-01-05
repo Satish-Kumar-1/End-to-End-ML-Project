358d83bd7268113d6c0e64305c04444bae46dd03
Satish Kumar <satish.kumar.data.science@gmail.com>
2025-01-06 00:11:34 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,9 +1,9 @@
 import sys
 import logging
-import logger
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
-    _, _, exc_tb = error_detail.exc.info()
+    _, _, exc_tb = sys.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = f"Error occured in python script {file_name} line number {exc_tb.tb_lineno} error message {str(error)}"
 
