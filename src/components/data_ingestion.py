f9db798486088220c86a664e630723cbe0742db5
Satish Kumar <satish.kumar.data.science@gmail.com>
2025-01-07 15:29:26 +0530

    hyperparameter tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,9 @@ from dataclasses import dataclass
 
 from src.components.data_transformations import DataTransformer
 from src.components.data_transformations import DataTransformerConfig
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
+
+
 
 @dataclass
 class DataIngestionConfig:
@@ -53,5 +56,7 @@ if __name__ == "__main__":
     # obj.initiate_data_ingestion()
 
     data_transformation = DataTransformer()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
             
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
